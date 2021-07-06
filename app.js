2c0f9a79129c643216c0b11d3f97061d2603ce2b
franco estrella <francoestrella55@gmail.com>
2021-07-06 18:12:48 -0300

    modify CORS policy.

--- app.js
+++ app.js
@@ -15,7 +15,16 @@ app.use("/api", jobRoute);
 app.use("/api", peopleRoute);
 
 // CORS
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 // MIDDLEWARES
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
